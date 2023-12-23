1467d413c40ba9f66c6201d6ad151870a247d6b8
shohag <shohagmiah2100@gmail.com>
2023-12-23 21:53:17 +0600

    everything ok

--- components/SingleChat.tsx
+++ components/SingleChat.tsx
@@ -5,9 +5,11 @@ import { getAuthSession } from "@/libs/getAuthSession";
 import { FindOrCreateConversation } from "@/libs/FindOrCreateConversation";
 import Link from "next/link";
 import { prisma } from "@/libs/db"
+import { getCurrentUser } from "@/libs/getCurrentUser";
 
 const SingleChat = async ({ conversation }: { conversation: Conversation }) => {
   const session = await getAuthSession();
+  const currentUser = await getCurrentUser();
 
 
   const lastMessage = await prisma.conversation.findFirst({
@@ -26,15 +28,17 @@ const SingleChat = async ({ conversation }: { conversation: Conversation }) => {
     }
   })
 
+  const otherUser = lastMessage?.userOne.id === currentUser?.id ? lastMessage?.userTwo : lastMessage?.userOne
+
   return (
     <Link
       href={`/chats/${conversation?.id}`}
       className="flex items-center justify-start md:justify-start py-2 px-4 gap-4 mt-2 rounded shadow hover:opacity-50 transition"
     >
-      <Avatar user={lastMessage?.userTwo} session={session} url={lastMessage?.userTwo.image || ""} />
+      <Avatar user={otherUser} session={session} url={otherUser?.image || ""} />
       <div className="flex items-start justify-center flex-col">
         <p className="text-lg  text-slate-300 capitalize font-semibold">
-          {lastMessage?.userTwo.name}
+          {otherUser?.name}
         </p>
           {!lastMessage?.messages[0]?.text && <p className="text-xl md:text-xs font-thin  text-slate-400">welcome, start a conversation</p>}
         <small className="text-xl md:text-xs font-thin  text-slate-400">
